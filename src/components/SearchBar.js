9be4126c69a1d2c25c6c403842bf10af06c886ae
Nigel Marshal <nigelmarshal@gmail.com>
2018-06-23 16:33:33 +0400

    Base structure

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,19 @@
 import React, { Component } from "react";
 
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { term: "" };
+  }
   render() {
-    return <div>Poo</div>;
+    return (
+      <div>
+        <input
+          value={this.state.term}
+          onChange={event => this.setState({ term: event.target.value })}
+        />
+      </div>
+    );
   }
 }
 
